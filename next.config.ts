0b95a99b2cd78e210b86d6b15124366bc7c93c5c
ppxb <317842449@qq.com>
2026-04-15 21:37:27 +0800

    chore: add next security config

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,66 @@
 import type { NextConfig } from 'next'
 
+const ContentSecurityPolicy = `
+  object-src 'none';
+  base-uri 'self';
+  frame-ancestors 'self';
+  manifest-src 'self';
+  report-to default;
+`
+
+const securityHeaders = [
+  {
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on'
+  },
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=63072000; includeSubDomains; preload'
+  },
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block'
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff'
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'no-referrer-when-downgrade'
+  },
+  {
+    key: 'Permissions-Policy',
+    value: `accelerometer=(), camera=(), gyroscope=(), microphone=(), usb=()`
+  },
+  {
+    key: 'Content-Security-Policy',
+    value: ContentSecurityPolicy.replace(/\n/g, '')
+  }
+]
+
 const nextConfig: NextConfig = {
+  poweredByHeader: false,
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: securityHeaders
+      }
+    ]
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**'
+      },
+      {
+        protocol: 'http',
+        hostname: '**'
+      }
+    ]
+  },
   async redirects() {
     return [
       {
@@ -9,7 +69,12 @@ const nextConfig: NextConfig = {
         permanent: true
       }
     ]
-  }
+  },
+  experimental: {
+    turbopackFileSystemCacheForBuild: true
+  },
+  reactStrictMode: true,
+  reactCompiler: true
 }
 
 export default nextConfig
