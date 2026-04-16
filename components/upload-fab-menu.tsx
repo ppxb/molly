5ee43183a31c7673626b9836b99214e53466adc7
ppxb <317842449@qq.com>
2026-04-16 18:41:00 +0800

    fix: select file not show upload panel

--- components/upload-fab-menu.tsx
+++ components/upload-fab-menu.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React from 'react'
+import React, { useRef } from 'react'
 import { FilePlusCornerIcon, FolderPlusIcon, FolderUpIcon, PlusIcon } from 'lucide-react'
 
 import { Button } from '@/components/ui/button'
@@ -17,6 +17,8 @@ interface UploadFabMenuProps {
 }
 
 export function UploadFabMenu({ onSelectFiles }: UploadFabMenuProps) {
+  const fileInputRef = useRef<HTMLInputElement>(null)
+
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const files = Array.from(event.target.files ?? [])
     if (files.length > 0) onSelectFiles(files)
@@ -24,33 +26,33 @@ export function UploadFabMenu({ onSelectFiles }: UploadFabMenuProps) {
   }
 
   return (
-    <div className="fixed right-12 bottom-12 z-40">
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Button size="icon-lg" className="shadow-md hover:cursor-pointer">
-            <PlusIcon className="size-5" />
-          </Button>
-        </DropdownMenuTrigger>
+    <>
+      <input ref={fileInputRef} type="file" multiple className="hidden" onChange={handleFileChange} />
+      <div className="fixed right-12 bottom-12 z-40">
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button size="icon-lg" className="shadow-md hover:cursor-pointer">
+              <PlusIcon className="size-5" />
+            </Button>
+          </DropdownMenuTrigger>
 
-        <DropdownMenuContent align="end" className="w-44">
-          <DropdownMenuLabel>添加到</DropdownMenuLabel>
-          <DropdownMenuItem asChild>
-            <label className="cursor-pointer">
+          <DropdownMenuContent align="end" className="w-44">
+            <DropdownMenuLabel>添加到</DropdownMenuLabel>
+            <DropdownMenuItem onSelect={() => fileInputRef.current?.click()}>
               <FilePlusCornerIcon className="size-4" />
               上传文件
-              <input type="file" multiple className="hidden" onChange={handleFileChange} />
-            </label>
-          </DropdownMenuItem>
-          <DropdownMenuItem disabled>
-            <FolderUpIcon className="size-4" />
-            上传文件夹
-          </DropdownMenuItem>
-          <DropdownMenuItem disabled>
-            <FolderPlusIcon className="size-4" />
-            新建文件夹
-          </DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
-    </div>
+            </DropdownMenuItem>
+            <DropdownMenuItem disabled>
+              <FolderUpIcon className="size-4" />
+              上传文件夹
+            </DropdownMenuItem>
+            <DropdownMenuItem disabled>
+              <FolderPlusIcon className="size-4" />
+              新建文件夹
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+    </>
   )
 }
