b2836e1315a342b31d090a8213955bc06c246bc1
ppxb <317842449@qq.com>
2026-04-16 11:37:25 +0800

    chore: add provider and metadata

---
+++ components/provider.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import type { ReactNode } from 'react'
+import { ThemeProvider } from 'next-themes'
+import { Toaster } from 'sonner'
+
+export function Providers({ children }: { children: ReactNode }) {
+  return (
+    <ThemeProvider attribute="class" enableSystem={true} disableTransitionOnChange>
+      {children}
+      <Toaster />
+    </ThemeProvider>
+  )
+}
